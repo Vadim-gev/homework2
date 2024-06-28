9b598f37f654be9a016a429cba982f0cde1c533b
Vadim-gev <vadim_gevorkian@mail.ru>
2024-06-28 14:24:09 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,10 @@
+homework = 12
+hours = 1.5
+course = 'Python'
+average = 1.5 / 12
+a = 'Курс:'
+b = ', всего задач:'
+c = ', затрачено часов: '
+d = ', среднее время выплнения: '
+e = ' часа.'
+print(a + course + b, homework, c, hours, d, average, e)
